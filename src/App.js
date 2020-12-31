e1a4f7088c0bb5217a349dbd036c8a66b55cbfbb
Daffa Z <dzcoc123@gmail.com>
2020-12-31 16:11:47 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Card from "./components/Card";
+import Search from "./components/Search";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setImages(data.hits);
+        setisLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, [term]);
+
+  console.log(term);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="md:container md:mx-auto flex flex-col items-center">
+      <Search keyword={(key) => setTerm(key)} />
+      {!isLoading && images.length === 0 && (
+        <span className="text-4xl mt-20">Images not found...</span>
+      )}
+      {isLoading ? (
+        <span className="text-6xl ">Loading...</span>
+      ) : (
+        <div className="my-14 flex flex-col items-center md:grid md:grid-cols-3 md:gap-4 ">
+          {images.map((image) => (
+            <Card key={image.id} image={image} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
